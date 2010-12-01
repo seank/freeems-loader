00ed5537cda6474dc5eb061f19f697077e7d24f1
Sean Keys <info@powerefi.com>
2010-11-30 22:40:17 -0700

    added srec docs

--- FreeEMS_LoaderSREC.cpp
+++ FreeEMS_LoaderSREC.cpp
@@ -7,6 +7,13 @@
 
 #include "FreeEMS_LoaderSREC.h"
 
+/*
+	+-------------------//------------------//-----------------------------+
+	| type | count | address  |            data                 | checksum |
+	+-------------------//------------------//-----------------------------+
+	   S2     14      F880B0    B746B7C687B7467D3044FD30421AEEFD      60
+*/
+
 FreeEMS_LoaderSREC::FreeEMS_LoaderSREC() {
 	// TODO Auto-generated constructor stub
 
--- FreeEMS_LoaderSREC.h
+++ FreeEMS_LoaderSREC.h
@@ -8,12 +8,27 @@
 #ifndef FREEEMS_LOADERSREC_H_
 #define FREEEMS_LOADERSREC_H_
 
+//enum BOOL {READONLY, READWRITE}
+
 class FreeEMS_LoaderSREC {
 public:
 	FreeEMS_LoaderSREC();
 	virtual ~FreeEMS_LoaderSREC();
-	bool verifyFile(int *file);
-	bool verifyRecord(char *buffer);
+//	bool verifyFile(int *file);
+	int  fillRecord(char *input);
+	bool verifyRecord();
+	int	 getRecordAddress();
+	char getNextByte();
+	char getRecordType();
+	char calculateCheckSum();
+	int  putNextByte(char byte);
+	int  setRecordAddress(int address);
+	int  setRecordType(char *type);
+
+private:
+	char record[1024]; // should never exceede 515
+	int  recordIndex;
+	bool writeAccess;
 
 };
 
