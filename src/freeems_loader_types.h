509f672d420c923fd5ec78a435eff5898c0a4530
Sean Keys <info@powerefi.com>
2010-12-14 21:41:55 -0700

    updated types tables

--- src/freeems_loader_types.h
+++ src/freeems_loader_types.h
@@ -5,32 +5,43 @@
 #define EVEN	2
 #define MARK	3
 #define SPACE	4
+
 #define MAXSNINTEENPAYLOAD      0x14
 
-//typedef struct  {
-//	char *port;
-//	unsigned int  baudrate;
-//	unsigned int  databits;
-//	unsigned int  parity;
-//	unsigned int  stop;
-//	unsigned int  softwareHandshake;
-//	unsigned int  hardwareHandshake;
-//}serialComSettings;
+#define FTX512K4        0x0001
+#define EETX4K          0x0002
+
+#define FIXED_FLASH     0x0001
+#define EEPROM          0x0002
+#define PAGED_FLASH     0x0004
+
+struct dataRange{
+  char *association;
+  unsigned int startAddress;
+  unsigned int stopAddress;
+  unsigned int memType;
+};
 
 struct flashModuleInfo{
   char *name;
-  int startFlashAddress;
-  int endFlashAddress;
-  int startPagedFlashAddress;
-  int endPagedFlashAddress;
+  unsigned int flashModule;
+  unsigned int EEPROMModule;
   int numFlashBytes;
+};
+
+const struct dataRange dataRangeInfo[]
+{
+    {"S12XDP512", 0x4000, 0x7FFF, FIXED_FLASH},
+    {"S12XDP512", 0x8000, 0xBFFF, PAGED_FLASH},
+    {"S12XDP512", 0xC000, 0xFFFF, FIXED_FLASH},
+    {0,0,0,0}
 };
 
 const struct flashModuleInfo flashModuleTable[]
 {
-    {"S12XDP512", 0x4000, 0x7FFF, 0x8000, 0xBFFF, 0x80000},
-    {"S12C64", 0x4000, 0x7FFF, 0x8000, 0xBFFF, 0x10000}, // TODO get correct values
-    {0,0,0,0,0}
+    {"S12XDP512", FTX512K4, EETX4K, 0x80000},
+    {"S12C64", 0, 0, 0x10000}, // TODO get correct values
+    {0,0,0,0}
 };
 
 #endif
