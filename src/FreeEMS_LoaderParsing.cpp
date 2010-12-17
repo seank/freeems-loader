adeebae15af54f48d165f02bef106059eb456b08
Sean Keys <info@powerefi.com>
2010-12-17 14:21:30 -0700

    added intToAscii function

--- src/FreeEMS_LoaderParsing.cpp
+++ src/FreeEMS_LoaderParsing.cpp
@@ -7,11 +7,16 @@
 
 #include "FreeEMS_LoaderParsing.h"
 #include "freeems_loader_types.h"
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include "stdio.h"
+
+using namespace std;
 
 FreeEMS_LoaderParsing::FreeEMS_LoaderParsing()
 {
   // TODO Auto-generated constructor stub
-
 }
 
 FreeEMS_LoaderParsing::~FreeEMS_LoaderParsing()
@@ -29,4 +34,41 @@ int
 FreeEMS_LoaderParsing::calcuateNumRecordsNeeded(int flashBytes, int bytesInRecord)
 {
   return (int) flashBytes/bytesInRecord;
+}
+
+void
+FreeEMS_LoaderParsing::intToHexAscii(int number, char* buffer, unsigned char numBitsToConvert)
+{
+  unsigned char charNumber = 0;
+  int i;
+    for( i = numBitsToConvert / 4, *(buffer + (numBitsToConvert / 4)) = 0; i > 0; i--, number >>= 4)
+      {
+          buffer += (i - 1);
+          charNumber = (unsigned char)(number & 0x0F);
+          if(charNumber > 0 && charNumber < 10)
+            *buffer = charNumber + '0';
+          if(charNumber > 10 && charNumber < 16)
+            *buffer = charNumber + 'a';
+          if(charNumber == 0)
+            *buffer = '0';
+       }
+/* debug
+   int test = 10;
+    printf("\n about to return char(s) ");
+    while(test > 0)
+      {
+        if(*buffer == 0)
+          {
+            printf(" zero");
+          }
+        else
+          {
+          putchar(*(buffer++));
+          }
+        buffer++;
+        test--;
+      }
+    printf("\n");
+  */
+  return;
 }
